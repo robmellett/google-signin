fb1a298b460332de41f1352b6162cf6b8fa75591
Rob Mellett <dev@robmellett.com>
2023-01-18 15:42:51 +0700

    Wip

---
+++ src/GoogleSignInSocialite.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AdoreBeauty\GoogleSignIn;
+
+use SocialiteProviders\Manager\SocialiteWasCalled;
+class GoogleSignInSocialite
+{
+    /**
+     * Register the provider.
+     *
+     * @param SocialiteWasCalled $socialiteWasCalled
+     */
+    public function handle(SocialiteWasCalled $socialiteWasCalled)
+    {
+        $socialiteWasCalled->extendSocialite(
+            'google',
+            __NAMESPACE__.'\Provider'
+        );
+    }
+}
---
+++ src/Provider.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace AdoreBeauty\GoogleSignIn;
+
+use Illuminate\Support\Arr;
+use SocialiteProviders\Manager\OAuth2\AbstractProvider;
+use SocialiteProviders\Manager\OAuth2\User;
+class Provider extends AbstractProvider
+{
+    /**
+     * Unique Provider Identifier.
+     */
+    const IDENTIFIER = 'GOOGLE';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopes = [
+        'openid',
+        'profile',
+        'email',
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $scopeSeparator = ' ';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getAuthUrl($state)
+    {
+        return $this->buildAuthUrlFromBase(
+            'https://accounts.google.com/o/oauth2/auth',
+            $state
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenUrl()
+    {
+        return 'https://oauth2.googleapis.com/token';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getUserByToken($token)
+    {
+        $response = $this->getHttpClient()->get('https://www.googleapis.com/oauth2/v3/tokeninfo', [
+            'query' => [
+                'prettyPrint' => 'false',
+                'id_token' => $token,
+            ],
+            'headers' => [
+                'Accept' => 'application/json',
+                'Content-Type' => 'application/json',
+            ],
+        ]);
+
+        return json_decode($response->getBody(), true);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function mapUserToObject(array $user)
+    {
+        return (new User())->setRaw($user)->map([
+            'id' => $user['sub'],
+            'nickname' => Arr::get($user, 'nickname'),
+            'name' => Arr::get($user, 'name'),
+            'first_name' => trim($user['given_name']),
+            'last_name' => trim($user['family_name'] ?? ''),
+            'email' => $user['email'],
+            'avatar' => $user['picture'],
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function getTokenFields($code)
+    {
+        return array_merge(parent::getTokenFields($code), [
+            'grant_type' => 'authorization_code',
+        ]);
+    }
+}
+
