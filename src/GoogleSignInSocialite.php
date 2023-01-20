aa66e0ccf69b96909d3fd42fff4764f897c2dc50
Rob Mellett <dev@robmellett.com>
2023-01-20 11:18:29 +0700

    Use Provider identifier

--- src/GoogleSignInSocialite.php
+++ src/GoogleSignInSocialite.php
@@ -14,7 +14,7 @@ class GoogleSignInSocialite
     public function handle(SocialiteWasCalled $socialiteWasCalled)
     {
         $socialiteWasCalled->extendSocialite(
-            'google',
+            Provider::IDENTIFIER,
             __NAMESPACE__.'\Provider'
         );
     }
